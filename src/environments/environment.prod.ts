2f3755663e7c4a2f9bcdb72fa2bf5caa2f923b72
Christian Barrios <stianward@gmail.com>
2020-09-14 11:03:12 -0500

    carga imagenes aplicacion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,19 @@
 export const environment = {
-  production: true
+  production: false,
+
+firebase:{
+  production: true,
+  apiKey: "AIzaSyAjjs2wcrxDFkvB2xrGBKHstiBH4CUW5do",
+  authDomain: "proyecto-and-1a7c8.firebaseapp.com",
+  databaseURL: "https://proyecto-and-1a7c8.firebaseio.com",
+  projectId: "proyecto-and-1a7c8",
+  storageBucket: "proyecto-and-1a7c8.appspot.com",
+  messagingSenderId: "177149343928",
+  appId: "1:177149343928:web:dd0f2d31110a49f0f41c4d"
+
+
+}
+
+
+
 };
